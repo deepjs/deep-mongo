5c2bdde9211ab7cbdf1efdfef79726779b5e52cb
Philippe Delabye <phil@chemicalrules.net>
2014-03-10 16:00:36 +0100

    debug deep-mongo

--- index.js
+++ index.js
@@ -1,311 +1,305 @@
 "use strict";
-if(typeof define !== 'function'){
-	var define = require('amdefine')(module);
-}
-define(function(require)
+var deep = require("deepjs");
+require("deepjs/lib/stores/store-sheet");
+var mongo = require('mongodb'),
+ObjectID = require('bson/lib/bson/objectid').ObjectID,
+rqlToMongo = require("./rql-to-mongo");
+deep.store.Mongo = deep.compose.Classes(deep.Store, function(protocol, url, collectionName, schema, options){
+	if(schema)
+		this.schema = schema;
+	if(url)
+		this.url = url;
+	if(collectionName)
+		this.collectionName = collectionName;
+	if(options)
+		deep.utils.up(options, this);
+},
 {
-	var deep = require("deepjs");
-	var deep = require("deepjs/lib/stores/store-sheet");
-	var mongo = require('mongodb'),
-	ObjectID = require('bson/lib/bson/objectid').ObjectID,
-	rqlToMongo = require("./rql-to-mongo");
-	deep.store.Mongo = deep.compose.Classes(deep.Store, function(protocol, url, collectionName, schema, options){
-		if(schema)
-			this.schema = schema;
-		if(url)
-			this.url = url;
-		if(collectionName)
-			this.collectionName = collectionName;
-		if(options)
-			deep.utils.up(options, this);
-	},
-	{
-		url:null,
-		collectionName:null,
-		init:function(options){
-			if(this.initialised)
-				return this.initialised;
-			//console.time("mongo.init");
-			//console.log("MONGO STORE INIT ");
-			options = options || {};
-			var url = options.url || this.url;
-			if(!url)
-			{
-				this.initialised = deep.when(deep.errors.Store("Mongo failed to init : no url provided !"));
-				return this.initialised;
-			}
-			var collectionName = options.collectionName || this.collectionName;
-			var self = this;
-			//console.log("MONGO STORE INIT : try connect");
-			this.initialised = deep.wrapNodeAsynch(mongo, "connect", [url])
-			.done(function(db){
-				db = db[0];
-				self.db = function(){ return db; };
-				return deep.wrapNodeAsynch(db, "collection", [collectionName]);
-			})
-			.done(function(coll){
-				coll = coll[0];
-				self.collection = coll;
-				return self.ensureIndex({ id: 1 }, { unique: true });
-			})
-			.done(function(){
-				console.log("MONGO DB : initialised : ",url, collectionName);
-			})
-			.fail(function(err){
-				console.error('Failed to connect to mongo database ', url, ' - error: ', err.message);
-			});
+	url:null,
+	collectionName:null,
+	init:function(options){
+		if(this.initialised)
 			return this.initialised;
-		},
-		get: function(id, options){
-			//console.log("Mongo : get : ", id, options);
-			options = options || {};
-			if(id == 'schema')
-			{
-				if(this.schema && this.schema._deep_ocm_)
-					return this.schema("get");
-				return this.schema || {};
-			}
-			if(!id)
-				id = "";
-			if(!id || id[0] === "?")
-				return this.query(id.substring(1), options);
-
-			var search = { id:id };
-			var meta = {};
-			var q = "id="+id;
-			if(options.filter)
-			{
-				q += options.filter;
-				var x = rqlToMongo.parse(q, {});
-				meta = x[0];
-				search = x[1];
-			}
-
-			return deep.wrapNodeAsynch(this.collection, "findOne", [search, meta])
-			.done(function(obj){
-				obj = obj[0];
-				if (obj)
-					delete obj._id;
-				else
-					return deep.errors.NotFound("maybe ownership restriction");
-				return obj;
-			});
-		},
-		post: function(object, options)
+		//console.time("mongo.init");
+		//console.log("MONGO STORE INIT ");
+		options = options || {};
+		var url = options.url || this.url;
+		if(!url)
 		{
-			options = options || {};
-			var id = options.id || object.id;
-			if (!id)
-				id = object.id = options.id = ObjectID.createPk().toJSON();
-			var self = this;
-			return deep.wrapNodeAsynch(self.collection,"insert", [object])
-			.fail(function(err){
-				if(err.code == 11000)
-					return deep.errors.Conflict();
-			})
-			.done(function(obj){
-				if(obj && obj[0])
-					obj = obj[0][0];
-				if (obj)
-					delete obj._id;
-				else
-					return deep.errors.NotFound();
-				return obj;
-			});
-		},
-		put: function(object, options)
+			this.initialised = deep.when(deep.errors.Store("Mongo failed to init : no url provided !"));
+			return this.initialised;
+		}
+		var collectionName = options.collectionName || this.collectionName;
+		var self = this;
+		//console.log("MONGO STORE INIT : try connect");
+		this.initialised = deep.wrapNodeAsynch(mongo, "connect", [url])
+		.done(function(db){
+			db = db[0];
+			self.db = function(){ return db; };
+			return deep.wrapNodeAsynch(db, "collection", [collectionName]);
+		})
+		.done(function(coll){
+			coll = coll[0];
+			self.collection = coll;
+			return self.ensureIndex({ id: 1 }, { unique: true });
+		})
+		.done(function(){
+			console.log("MONGO DB : initialised : ",url, collectionName);
+		})
+		.fail(function(err){
+			console.error('Failed to connect to mongo database ', url, ' - error: ', err.message);
+		});
+		return this.initialised;
+	},
+	get: function(id, options){
+		//console.log("Mongo : get : ", id, options);
+		options = options || {};
+		if(id == 'schema')
 		{
-			options = options || {};
-			var id = options.id || object.id;
-			return deep.wrapNodeAsynch(this.collection, "findAndModify",[{id:id}, null /* sort */, object, {upsert:false, "new":true}])
-			.done(function(response){
-				response = response[0];
-				if (response)
-					delete response._id;
-				else
-					return deep.errors.NotFound();
-				return response;
-			});
-		},
-		patch: function(object, options)
+			if(this.schema && this.schema._deep_ocm_)
+				return this.schema("get");
+			return this.schema || {};
+		}
+		if(!id)
+			id = "";
+		if(!id || id[0] === "?")
+			return this.query(id.substring(1), options);
+
+		var search = { id:id };
+		var meta = {};
+		var q = "id="+id;
+		if(options.filter)
 		{
-			options = options || {};
-			var id = options.id || object.id;
-			return deep.wrapNodeAsynch(this.collection, "findAndModify", [{id: id}, null /* sort */, object, {upsert:false, "new":true}])
-			.done(function(response){
-				response = response[0];
-				if (response)
-					delete response._id;
-				else
-					return deep.errors.NotFound();
-				return response;
-			});
-		},
-		MAX_QUERY_LIMIT:500,
-		query: function(query, options)
+			q += options.filter;
+			var x = rqlToMongo.parse(q, {});
+			meta = x[0];
+			search = x[1];
+		}
+
+		return deep.wrapNodeAsynch(this.collection, "findOne", [search, meta])
+		.done(function(obj){
+			obj = obj[0];
+			if (obj)
+				delete obj._id;
+			else
+				return deep.errors.NotFound("maybe ownership restriction");
+			return obj;
+		});
+	},
+	post: function(object, options)
+	{
+		options = options || {};
+		var id = options.id || object.id;
+		if (!id)
+			id = object.id = options.id = ObjectID.createPk().toJSON();
+		var self = this;
+		return deep.wrapNodeAsynch(self.collection,"insert", [object])
+		.fail(function(err){
+			if(err.code == 11000)
+				return deep.errors.Conflict();
+		})
+		.done(function(obj){
+			if(obj && obj[0])
+				obj = obj[0][0];
+			if (obj)
+				delete obj._id;
+			else
+				return deep.errors.NotFound();
+			return obj;
+		});
+	},
+	put: function(object, options)
+	{
+		options = options || {};
+		var id = options.id || object.id;
+		return deep.wrapNodeAsynch(this.collection, "findAndModify",[{id:id}, null /* sort */, object, {upsert:false, "new":true}])
+		.done(function(response){
+			response = response[0];
+			if (response)
+				delete response._id;
+			else
+				return deep.errors.NotFound();
+			return response;
+		});
+	},
+	patch: function(object, options)
+	{
+		options = options || {};
+		var id = options.id || object.id;
+		return deep.wrapNodeAsynch(this.collection, "findAndModify", [{id: id}, null /* sort */, object, {upsert:false, "new":true}])
+		.done(function(response){
+			response = response[0];
+			if (response)
+				delete response._id;
+			else
+				return deep.errors.NotFound();
+			return response;
+		});
+	},
+	MAX_QUERY_LIMIT:500,
+	query: function(query, options)
+	{
+		options = options || {};
+		if(!query)
+			query = "";
+
+		var parsingDirectives = {};
+
+		var deferred = deep.Deferred();
+		var self = this;
+
+		var noRange = false;
+		if(!options.start && !options.end)
 		{
-			options = options || {};
-			if(!query)
-				query = "";
+			noRange = true;
+			options.start = 0;
+			options.end = this.MAX_QUERY_LIMIT;
+		}
+		options.start = options.start || 0;
+		options.end = options.end || 0;
+		if(options.end - options.start > this.MAX_QUERY_LIMIT)
+			options.end = options.start + this.MAX_QUERY_LIMIT;
+		query += "&limit("+((options.end-options.start)+1)+","+options.start+")";
 
-			var parsingDirectives = {};
+		if(options.filter)
+			query += options.filter;
+		if(query[0] == "?")
+			query = query.substring(1);
+		// compose search conditions
+		var x = rqlToMongo.parse(query, parsingDirectives);
+		var meta = x[0], search = x[1];
+		query = "?"+query;
 
-			var deferred = deep.Deferred();
-			var self = this;
+		if (meta.limit <= 0) {
+			var rangeObject = deep.utils.createRangeObject(0, 0, 0, 0, [], query);
+			rangeObject.results = [];
+			rangeObject.count = 0;
+			rangeObject.query = query;
+			return rangeObject;
+		}
 
-			var noRange = false;
-			if(!options.start && !options.end)
-			{
-				noRange = true;
-				options.start = 0;
-				options.end = this.MAX_QUERY_LIMIT;
-			}
-			options.start = options.start || 0;
-			options.end = options.end || 0;
-			if(options.end - options.start > this.MAX_QUERY_LIMIT)
-				options.end = options.start + this.MAX_QUERY_LIMIT;
-			query += "&limit("+((options.end-options.start)+1)+","+options.start+")";
+		var totalCountPromise = null;
+		if(!noRange)
+			totalCountPromise = this.count(search);
 
-			if(options.filter)
-				query += options.filter;
-			if(query[0] == "?")
-				query = query.substring(1);
+		var context = deep.context;
+		deep.wrapNodeAsynch(self.collection, "find", [search, meta])
+		.done(function(cursor){
+			cursor = cursor[0];
+			cursor.toArray(function(err, results){
+				if (err)
+					return deferred.reject(err);
+				if (results && results[0] && results[0].$err !== undefined && results[0]._id === undefined)
+					return deferred.reject(results[0].$err);
+				var fields = meta.fields;
+				var len = results.length;
+				// damn ObjectIDs!
+				for (var i = 0; i < len; i++) {
+					delete results[i]._id;
+				}
+				if(noRange)
+					return deferred.resolve(results);
+				deep.when(totalCountPromise)
+				.done(function (result){
+					//deep.context = context;
+					var rangeObject = deep.utils.createRangeObject(options.start,
+						Math.max(options.start,options.start+results.length-1),
+						result,
+						results.length,
+						results,
+						query
+					);
+					deferred.resolve(rangeObject);
+				})
+				.fail(function (error) {
+					deferred.reject(error);
+				});
+			});
+		})
+		.fail(function(e){
+			deferred.reject(e);
+		});
+		//________________________
+		//console.log("deep.stores.Mongo will do query : ", query);
+		return deferred.promise();
+	},
+	del: function(id, options){
+		var search = {id: id}, meta = null;
+		if(id[0] == "?")
+		{
+			id = id.substring(1);
 			// compose search conditions
-			var x = rqlToMongo.parse(query, parsingDirectives);
-			var meta = x[0], search = x[1];
-			query = "?"+query;
-
-			if (meta.limit <= 0) {
-				var rangeObject = deep.utils.createRangeObject(0, 0, 0, 0, [], query);
-				rangeObject.results = [];
-				rangeObject.count = 0;
-				rangeObject.query = query;
-				return rangeObject;
-			}
-
-			var totalCountPromise = null;
-			if(!noRange)
-				totalCountPromise = this.count(search);
-
-			var context = deep.context;
-			deep.wrapNodeAsynch(self.collection, "find", [search, meta])
-			.done(function(cursor){
-				cursor = cursor[0];
-				cursor.toArray(function(err, results){
-					if (err)
-						return deferred.reject(err);
-					if (results && results[0] && results[0].$err !== undefined && results[0]._id === undefined)
-						return deferred.reject(results[0].$err);
-					var fields = meta.fields;
-					var len = results.length;
-					// damn ObjectIDs!
-					for (var i = 0; i < len; i++) {
-						delete results[i]._id;
-					}
-					if(noRange)
-						return deferred.resolve(results);
-					deep.when(totalCountPromise)
-					.done(function (result){
-						//deep.context = context;
-						var rangeObject = deep.utils.createRangeObject(options.start,
-							Math.max(options.start,options.start+results.length-1),
-							result,
-							results.length,
-							results,
-							query
-						);
-						deferred.resolve(rangeObject);
-					})
-					.fail(function (error) {
-						deferred.reject(error);
-					});
-				});
-			})
-			.fail(function(e){
-				deferred.reject(e);
+			var x = rqlToMongo.parse(id, {});
+			meta = x[0];
+			search = x[1];
+		}
+		meta.safe = true;
+		return deep.wrapNodeAsynch(this.collection, "remove", [search, meta])
+		.done(function(s){
+			return s[0] === 1;
+		});
+	},
+	range:function(start, end, query)
+	{
+		return this.query(query || "", { start:start, end:end });
+	},
+	flush:function(options){
+		var self = this;
+		return this.init()
+		.done(function(){
+			return deep.wrapNodeAsynch(self.collection, "drop", [])
+			.done(function(done){
+				return self.ensureIndex({ id: 1 }, { unique: true });
 			});
-			//________________________
-			//console.log("deep.stores.Mongo will do query : ", query);
-			return deferred.promise();
-		},
-		del: function(id, options){
-			var search = {id: id}, meta = null;
-			if(id[0] == "?")
-			{
-				id = id.substring(1);
-				// compose search conditions
-				var x = rqlToMongo.parse(id, {});
-				meta = x[0];
-				search = x[1];
-			}
-			meta.safe = true;
-			return deep.wrapNodeAsynch(this.collection, "remove", [search, meta])
-			.done(function(s){
-				return s[0] === 1;
-			});
-		},
-		range:function(start, end, query)
-		{
-			return this.query(query || "", { start:start, end:end });
-		},
-		flush:function(options){
-			var self = this;
-			return this.init()
-			.done(function(){
-				return deep.wrapNodeAsynch(self.collection, "drop", [])
-				.done(function(done){
-					return self.ensureIndex({ id: 1 }, { unique: true });
-				});
-			})
-			.fail(function(e){
-				console.log("flush error : ", e);
-			});
-		},
-		indexes:function(){
-			var self = this;
-			return this.init()
-			.done(function(success){
-				return deep.wrapNodeAsynch(self.collection, "indexes", [])
-				.done(function(indexes){
-					return indexes[0];
-				});
-			});
-		},
-		reIndex:function(){
-			var self = this;
-			return this.init()
-			.done(function(success){
-				return deep.wrapNodeAsynch(self.collection, "reIndex", [])
-				.done(function(indexes){
-					return indexes[0];
-				});
-			});
-		},
-		ensureIndex:function (properties, options) {
-			var self = this;
-			return this.init()
-			.done(function(success){
-				return deep.wrapNodeAsynch(self.collection, "ensureIndex", [properties, options]);
-			})
+		})
+		.fail(function(e){
+			console.log("flush error : ", e);
+		});
+	},
+	indexes:function(){
+		var self = this;
+		return this.init()
+		.done(function(success){
+			return deep.wrapNodeAsynch(self.collection, "indexes", [])
 			.done(function(indexes){
 				return indexes[0];
 			});
-		},
-		count:function (arg){
-			return deep.wrapNodeAsynch(this.collection, "count", [arg])
-			.done(function(totalCount){
-				return totalCount[0];
+		});
+	},
+	reIndex:function(){
+		var self = this;
+		return this.init()
+		.done(function(success){
+			return deep.wrapNodeAsynch(self.collection, "reIndex", [])
+			.done(function(indexes){
+				return indexes[0];
 			});
-		}
-	});
-
-	deep.sheet(deep.store.fullSheet, deep.store.Mongo.prototype);
-	deep.store.Mongo.create = function(protocol, url, collection, schema, options){
-		return new deep.store.Mongo(protocol, url, collection, schema, options);
-	};
-
-	deep.coreUnits = deep.coreUnits || [];
-	deep.coreUnits.push("js::deep-mongo/units/generic");
-
-	return deep.store.Mongo;
+		});
+	},
+	ensureIndex:function (properties, options) {
+		var self = this;
+		return this.init()
+		.done(function(success){
+			return deep.wrapNodeAsynch(self.collection, "ensureIndex", [properties, options]);
+		})
+		.done(function(indexes){
+			return indexes[0];
+		});
+	},
+	count:function (arg){
+		return deep.wrapNodeAsynch(this.collection, "count", [arg])
+		.done(function(totalCount){
+			return totalCount[0];
+		});
+	}
 });
+
+deep.sheet(deep.store.fullSheet, deep.store.Mongo.prototype);
+deep.store.Mongo.create = function(protocol, url, collection, schema, options){
+	return new deep.store.Mongo(protocol, url, collection, schema, options);
+};
+
+deep.coreUnits = deep.coreUnits || [];
+deep.coreUnits.push("js::deep-mongo/units/generic");
+
+module.exports = deep.store.Mongo;
